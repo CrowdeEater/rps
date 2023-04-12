026ba0dc9efc03f50fbd0f48f3b2b04246ca1cbd
CrowdeEater <talikk88@gmail.com>
2023-04-12 01:05:34 -0700

    Create score values. Create random value generator. Create if/else logic for game.

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+let playerScore = 0;
+let computerScore = 0;
+let rounds = 0;
+
+//get random choice for Computer.
+function getComputerChoice() {
+        let choice = ['rock', 'paper', 'scissors'];
+        const randomChoice = choice[Math.floor(Math.random() * choice.length)];
+        return randomChoice;
+}
+
+//play one round of the game
+function playRound(playerSelection, computerSelection) {
+    if(playerSelection.toUpperCase() === computerSelection.toUpperCase()) {
+        document.getElementById('result').textContent = "It's a draw!";
+    }
+    else if((playerSelection.toUpperCase() === 'ROCK' && computerSelection.toUpperCase() === 'PAPER') ||
+    (playerSelection.toUpperCase() === 'PAPER' && computerSelection.toUpperCase() === 'SCISSORS') ||
+    (playerSelection.toUpperCase() === 'SCISSORS' && computerSelection.toUpperCase() === 'ROCK')) {
+
+        computerScore++;
+        document.getElementById('result').textContent = "You Lose!";
+    } else {
+        playerScore++;
+        document.getElementById('result'),textContent = "You Won!";
+    }
+}
 
