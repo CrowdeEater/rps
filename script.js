fb03b243bfb28bb257532af835c302221a3470fb
CrowdeEater <talikk88@gmail.com>
2023-04-19 14:34:54 -0700

    basic running - but bugged - build

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function playRound(playerSelection, computerSelection) {
     }
     else if((playerSelection.toUpperCase() === 'ROCK' && computerSelection.toUpperCase() === 'PAPER') ||
     (playerSelection.toUpperCase() === 'PAPER' && computerSelection.toUpperCase() === 'SCISSORS') ||
-    (playerSelection.toUpperCase() === 'SCISSORS' && computerSelection.toUpperCase() === 'ROCK')) {
+    (playerSelection.toUpperCase() === 'SCISSORS' && computerSelection.toUpperCase() === 'ROCK')){
 
         computerScore++;
         document.getElementById('result').textContent = "You Lose!";
@@ -26,3 +26,45 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
+
+function checkWinner() {
+    if(rounds === 5) {
+        if(computerScore === playerScore) {
+            document.getElementById('result').textContent = "You\'ve ended in a draw!";
+        }
+        else if (playerScore > computerScore) {
+            document.getElementById('result').textContent = "You\'ve won the game!";
+        }
+        else {
+            document.getElementById('result').textContent = "You\'ve lost the game :(";
+        }
+        return true;
+    }
+    return false;
+}
+
+function updateScore(playerSelection, computerSelection) {
+    document.getElementById('pSelection').textContent = playerSelection;
+    document.getElementById('cSelection').textContent = computerSelection;
+    document.getElementById('playerScore').textContent = playerScore;
+    document.getElementById('computerScore').textContent = computerScore;
+}
+
+function game() {
+    const options = document.querySelectorAll('.btn');
+    options.forEach(option => option.addEventListener('click', () => {
+        const computerSelection = getComputerChoice();
+        const playerSelection = option.id;
+
+        document.getElementById('result').textContent = '';
+
+        playRound(playerSelection, computerSelection);
+        updateScore(playerSelection, computerSelection);
+        rounds++;
+        if(checkWinner()) {
+            rounds = computerScore = playerScore = 0;
+            updateScore(playerSelection, computerSelection);
+        };
+    }));
+}
+game();
